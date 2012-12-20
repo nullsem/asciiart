ea1cc18ddcccd8ce716d3a3670f3e15f10ce0d50
nullsem <nullsem@gmail.com>
2012-12-20 11:12:40 +0200

    added colors.py bash RGB support

---
+++ asciiart/colors.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+class colors:
+    def __init__(self):
+        self.colors = {
+                'black'     : '0;30',
+                'red'       : '0:31',
+                'green'     : '0:32',
+                'brown'     : '0;33',
+                'blue'      : '0;34',
+                'purple'    : '0;35',
+                'cyan'      : '0;36',
+                'light_gray': '0;37',
+                'dark_gray' : '1;30',
+
+
